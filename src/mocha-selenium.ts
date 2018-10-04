7bbeda33ff5696543093b5436f6a7f7ea4d29b06
zbigg <z.zagorski@gmail.com>
2018-10-04 23:18:32 +0200

    use mocha.Runner as event emitter for reporters (fixes tap reporter)

--- src/mocha-selenium.ts
+++ src/mocha-selenium.ts
@@ -1,101 +1,29 @@
-import { EventEmitter } from "events";
+import * as commander from "commander";
+import * as fs from "fs";
 import * as mocha from "mocha";
+import * as path from "path";
 import { Builder, WebDriver } from "selenium-webdriver";
+
 import { fetchPageEvents } from "./page-event-queue";
-
-/*
-class FakeRunner extends EventEmitter  implements Mocha.Runner {
-    suite: mocha.Suite;
-    started: boolean;
-    total: number;
-    failures: number;
-    asyncOnly?: boolean | undefined;
-    allowUncaught?: boolean | undefined;
-    fullStackTrace?: boolean | undefined;
-    forbidOnly?: boolean | undefined;
-    forbidPending?: boolean | undefined;
-    ignoreLeaks?: boolean | undefined;
-    test?: mocha.Test | undefined;
-    currentRunnable?: mocha.Runnable | undefined;
-    stats?: mocha.Stats | undefined;
-
-    grep(re: RegExp, invert: boolean): this {
-        throw new Error("Method not implemented.");
-    }
-    grepTotal(suite: mocha.Suite | mocha.ISuite): number {
-        throw new Error("Method not implemented.");
-    }
-    globals(): string[];
-    globals(arr: ReadonlyArray<string>): this;
-    globals(arr?: any): any {
-        throw new Error("Method not implemented.");
-    }
-    run(fn?: ((failures: number) => void) | undefined): this {
-        throw new Error("Method not implemented.");
-    }
-    abort(): this {
-        throw new Error("Method not implemented.");
-    }
-    uncaught(err: any): void {
-        throw new Error("Method not implemented.");
-    }
-    protected globalProps(): string[] {
-        throw new Error("Method not implemented.");
-    }
-    protected checkGlobals(test: mocha.Test): void {
-        throw new Error("Method not implemented.");
-    }
-    protected fail(test: mocha.Test, err: any): void {
-        throw new Error("Method not implemented.");
-    }
-    protected failHook(hook: mocha.Hook, err: any): void {
-        throw new Error("Method not implemented.");
-    }
-    protected hook(name: string, fn: () => void): void {
-        throw new Error("Method not implemented.");
-    }
-    protected hooks(name: string, suites: mocha.Suite[], fn: (err?: any, errSuite?: mocha.Suite | undefined) => void): void {
-        throw new Error("Method not implemented.");
-    }
-    protected hookUp(name: string, fn: (err?: any, errSuite?: mocha.Suite | undefined) => void): void {
-        throw new Error("Method not implemented.");
-    }
-    protected hookDown(name: string, fn: (err?: any, errSuite?: mocha.Suite | undefined) => void): void {
-        throw new Error("Method not implemented.");
-    }
-    protected parents(): mocha.Suite[] {
-        throw new Error("Method not implemented.");
-    }
-    protected runTest(fn: mocha.Done) {
-        throw new Error("Method not implemented.");
-    }
-    protected runTests(suite: mocha.Suite, fn: (errSuite?: mocha.Suite | undefined) => void): void {
-        throw new Error("Method not implemented.");
-    }
-    protected runSuite(suite: mocha.Suite, fn: (errSuite?: mocha.Suite | undefined) => void): void {
-        throw new Error("Method not implemented.");
-    }
-}
-
-*/
+import { createMochaStateSynchronizer } from "./suite-synchronizer";
 
 async function withWebDriver<T>(test: (driver: WebDriver) => T) {
     let theDriver: WebDriver | undefined;
     return Promise.resolve(
         new Builder().build().then(async driver => {
             theDriver = driver;
-            // console.log("driver connected");
             const result = await test(driver);
-            // console.log("ok!");
             await driver.quit();
             theDriver = undefined;
             return result;
         })
     ).catch(async (error: Error) => {
-        // ensure that driver always quits
-        // console.error("croak", error);
         if (theDriver !== undefined) {
-            await theDriver.quit();
+            try {
+                await theDriver.quit();
+            } catch(error) {
+                console.log('withWebDriver: error while quiting WebDriver session', error);
+            }
         }
         throw error;
     });
@@ -104,6 +32,7 @@ async function withWebDriver<T>(test: (driver: WebDriver) => T) {
 interface Options {
     reporter?: string;
     reporterOptions?: any;
+    delay?: boolean;
 }
 
 function getReporterConstructor(options: Options) {
@@ -132,11 +61,10 @@ function getReporterConstructor(options: Options) {
 async function runMochaTest(url: string, options: Options): Promise<boolean> {
     const synchronizer = createMochaStateSynchronizer();
 
-    const runnerEventEmitter: any = new EventEmitter();
-
-    // consr runner = new Mocha.Runner()
     const reporterConstructor = getReporterConstructor(options);
-    const reporter = new reporterConstructor(runnerEventEmitter as mocha.Runner, options);
+
+    let runner: mocha.Runner | undefined;
+    let reporter: mocha.Reporter | undefined;
 
     return withWebDriver(async driver => {
         await driver.get(url);
@@ -147,26 +75,28 @@ async function runMochaTest(url: string, options: Options): Promise<boolean> {
             const events = await fetchPageEvents(driver);
             for (const event of events) {
                 if (event.type === "start") {
-                    runnerEventEmitter.suite = synchronizer.recv(event.suite);
-                    runnerEventEmitter.emit("start");
+                    const suite = synchronizer.recv(event.suite);
+                    runner = new mocha.Runner(suite, options.delay === true);
+                    reporter = new reporterConstructor(runner, options);
+                    runner.emit("start");
                 } else if (event.type === "suite") {
-                    runnerEventEmitter.emit("suite", synchronizer.recv(event.suite));
+                    runner!.emit("suite", synchronizer.recv(event.suite));
                 } else if (event.type === "suite end") {
-                    runnerEventEmitter.emit("suite end");
+                    runner!.emit("suite end");
                 } else if (event.type === "test end") {
-                    runnerEventEmitter.emit("test end", synchronizer.recv(event.test));
+                    runner!.emit("test end", synchronizer.recv(event.test));
                 } else if (event.type === "pass") {
-                    runnerEventEmitter.emit("pass", synchronizer.recv(event.test));
+                    runner!.emit("pass", synchronizer.recv(event.test));
                 } else if (event.type === "fail") {
-                    runnerEventEmitter.emit("fail", synchronizer.recv(event.test), synchronizer.recv(event.err));
+                    runner!.emit("fail", synchronizer.recv(event.test), synchronizer.recv(event.err));
                 } else if (event.type === "end") {
-                    runnerEventEmitter.emit("end");
+                    runner!.emit("end");
                     finished = true;
                     exitCode = event.failures === 0 ? 0 : 1;
                 } else if (event.type === "pending") {
-                    runnerEventEmitter.emit("pending", synchronizer.recv(event.test));
+                    runner!.emit("pending", synchronizer.recv(event.test));
                 } else {
-                    console.error("invalid command", event.type || event);
+                    console.error("invalid event received from page", event.type || event);
                 }
             }
         }
@@ -196,11 +126,6 @@ function parseReporterOptions(optionsString?: string) {
     return result;
 }
 
-//const commander = require("commander");
-import * as commander from "commander";
-import * as fs from "fs";
-import * as path from "path";
-import { createMochaStateSynchronizer } from "./suite-synchronizer";
 
 const version = JSON.parse(fs.readFileSync(path.join(__dirname, "..", "package.json"), "utf8")).version;
 
@@ -217,6 +142,7 @@ if (args.length < 1) {
     commander.outputHelp();
     throw new Error("mocha-selenium: URL needed");
 }
+
 const url = commander.args.shift()!;
 const cliOptions = commander.opts();
 const options = {
