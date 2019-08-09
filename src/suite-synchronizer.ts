7d95fc4c7616d85fda6a51edccc70124af3bc43a
zbigg <z.zagorski@gmail.com>
2019-08-09 02:33:29 +0200

    support for hooks

--- src/suite-synchronizer.ts
+++ src/suite-synchronizer.ts
@@ -4,6 +4,10 @@ import * as mocha from "mocha";
 const MOCHA_SUITE_SYNCHRONIZED_PROPERTIES = [
     "title",
     "tests",
+    "_beforeAll",
+    "_afterAll",
+    "_beforeEach",
+    "_afterEach",
     "suites",
     "pending",
     "passes",
@@ -31,13 +35,16 @@ const MOCHA_TEST_SYNCHRONIZED_PROPERTIES = [
     "skipped",
     "duration",
     "currentRetry",
-    "context"
+    "context",
+    "ctx"
 ];
+
+const MOCHA_HOOK_SYNCHRONIZED_PROPERTIES = ["title", "type", "state", "duration", "parent", "fn", "ctx", "file"];
 
 export function createMochaStateSynchronizer(): Synchronizer {
     const synchronizer = new Synchronizer();
     if (typeof mocha === "undefined") {
-        throw new Error('mocha not loaded, cannot create Mocha Suite/Test synchronizer');
+        throw new Error("mocha not loaded, cannot create Mocha Suite/Test synchronizer");
     }
 
     synchronizer.serializationContext.addClass({
@@ -59,6 +66,23 @@ export function createMochaStateSynchronizer(): Synchronizer {
         },
         propertyFilter: (name: string) => {
             return MOCHA_TEST_SYNCHRONIZED_PROPERTIES.includes(name);
+        },
+        propertyMapSerialize: (name: string, value: any) => {
+            if (name === "fn" && typeof value === "function") {
+                return value.toString();
+            }
+            return value;
+        }
+    });
+
+    synchronizer.serializationContext.addClass({
+        name: "Mocha.Hook",
+        constructor: mocha.Hook as any,
+        factory: function() {
+            return new mocha.Hook("dummy-hook");
+        },
+        propertyFilter: (name: string) => {
+            return MOCHA_HOOK_SYNCHRONIZED_PROPERTIES.includes(name);
         },
         propertyMapSerialize: (name: string, value: any) => {
             if (name === "fn" && typeof value === "function") {
